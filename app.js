74d556aa19a0c6d04f9730ba1aa3de530f42c812
SunBowei <809340810@qq.com>
2016-06-28 17:16:37 +0800

    Update app.js
    增加ejs模板的引用和设置

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +12,13 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.set('views', __dirname + '/views');
+app.engine('.html',ejs.__express);
+app.set('view engine', 'html');
+/*
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
