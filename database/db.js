92653f7ae5721036a63850923703642852968276
SunBowei <809340810@qq.com>
2016-06-29 15:43:58 +0800

    连接数据库
    连接数据库的文件，其中运用了mongoose模块进行模型创建

--- database/db.js
+++ database/db.js
@@ -5,4 +5,4 @@ var userScheMa = new Schema({
     name:String,
     password:String
 });   //定义了一个新的模型，但是此模式还未和users集合有关联
-exports.user = db.model('blog_user',userScheMa);    //与users集合关联
+exports.user = db.model('blog_user',userScheMa);    //与blog_user集合关联
