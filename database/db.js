4415b95138c8c991142c340e73cf3dcae18dc297
SunBowei <809340810@qq.com>
2016-07-04 15:17:25 +0800

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var db = mongoose.connect('123.57.50.14');   //连接数据库
 var schema = mongoose.Schema;   //创建模型
-var userScheMa = new Schema({
+var userScheMa = new schema({
     name:"admin",
     password:"123456"
 });   //定义了一个新的模型，但是此模式还未和users集合有关联
