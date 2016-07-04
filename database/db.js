318af193b36b548987e9f6cff4d26af6a59c561d
SunBowei <809340810@qq.com>
2016-07-04 15:29:55 +0800

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ var db = mongoose.connect('mongodb://123.57.50.14:27017/my_blog');   //连接数
 
 var schema = mongoose.Schema;   //创建模型
 var userScheMa = new schema({
-    name:"sunbowei",
-    password:"123456"
+    name:String,
+    password:String
 });   //定义了一个新的模型，但是此模式还未和users集合有关联
 exports.user = db.model('blog_user',userScheMa);    //与blog_user集合关联
