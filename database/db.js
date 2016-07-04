13365d908168838af8790e94937dd8979f11871b
SunBowei <809340810@qq.com>
2016-07-04 15:12:53 +0800

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var db = mongoose.connect('123.57.50.14');   //连接数据库
 var schema = mongoose.Schema;   //创建模型
 var userScheMa = new Schema({
-    name:String,
-    password:String
+    name:"admin",
+    password:"123456"
 });   //定义了一个新的模型，但是此模式还未和users集合有关联
 exports.user = db.model('blog_user',userScheMa);    //与blog_user集合关联
