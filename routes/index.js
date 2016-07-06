6ed7865d97593233b26738eeab11406f7e1d56a9
opacitySun <809340810@qq.com>
2016-07-06 11:54:28 +0800

    更新修改

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/login', function(req, res) {
 });
 
  /* ucenter */
+ /*
 router.post('/ucenter', function(req, res) {
 	  var query = {name: req.body.name, password: req.body.password};
 	  (function(){
@@ -20,6 +21,21 @@ router.post('/ucenter', function(req, res) {
 					if(doc == 1){
 						console.log(query.name + ": 登陆成功 " + new Date());
 						res.render('ucenter', { title:'ucenter' });
+					}else{
+						console.log(query.name + ": 登陆失败 " + new Date());
+						res.redirect('/');
+					}
+		  	});
+	  })(query);
+});
+*/
+router.post('/index', function(req, res) {
+	  var query = {name: req.body.login_name, password: req.body.login_pwd};
+	  (function(){
+			  user.count(query, function(err, doc){    //count返回集合中文档的数量，和 find 一样可以接收查询条件。query 表示查询的条件
+					if(doc == 1){
+						console.log(query.name + ": 登陆成功 " + new Date());
+						res.render('index', { title:'index' });
 					}else{
 						console.log(query.name + ": 登陆失败 " + new Date());
 						res.redirect('/');
