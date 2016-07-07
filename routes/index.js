7be5a77a45af8dec47dbf4561354808d8c89707d
opacitySun <809340810@qq.com>
2016-07-07 09:55:26 +0800

    更新修改

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var user = require('../database/db').user;
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
@@ -10,22 +9,6 @@ router.get('/index', function(req, res, next) {
 /* login */
 router.get('/login', function(req, res) {
     res.render('login', { title: 'login' });
-});
-
- /* ucenter */
-router.post('/index', function(req, res) {
-	  var query = {name: req.body.login_name, password: req.body.login_pwd};
-	  (function(){
-			  user.count(query, function(err, doc){    //count返回集合中文档的数量，和 find 一样可以接收查询条件。query 表示查询的条件
-					if(doc == 1){
-						console.log(query.name + ": 登陆成功 " + new Date());
-						res.render('index', { title:'ucenter' });
-					}else{
-						console.log(query.name + ": 登陆失败 " + new Date());
-						res.redirect('/');
-					}
-		  	});
-	  })(query);
 });
 
 module.exports = router;
