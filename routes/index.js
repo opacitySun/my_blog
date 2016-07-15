479dcaf640e48c6c3d3b5c38a6a03c7926691e29
opacitySun <809340810@qq.com>
2016-07-15 17:06:17 +0800

    更新修改

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,26 @@ router.get('/login', function(req, res) {
 
  /* ucenter */
 router.post('/ucenter', function(req, res) {
-	userController.userFindAction(req, res);
+	var  mongodb = require('mongodb');
+	var  server  = new mongodb.Server('123.57.50.14', 27017, {auto_reconnect:true});
+	var  db = new mongodb.Db('my_blog', server, {safe:true});
+	db.open(function(err,db){
+		if(!err){
+			console.log('connect db');
+			db.createCollection('blog_user', {safe:true}, function(table_err, collection){
+	            if(table_err){
+	                console.log(table_err);
+	            }else{
+	                collection.find({'name':req.body.login_name,'password':req.body.login_pwd},function(error,result){
+	                	console.log(JSON.stringify(result));
+	                }); 
+	            }
+        	});
+		}else{
+			console.log(err);
+		}
+	});
+	//userController.userFindAction(req, res);
 	/*
 	(function(){
 		user.count(query, function(err, result){    //result:0是请求成功，1是请求失败
