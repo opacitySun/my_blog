5d4da6f83899af6ed2bc951209a2ffda58e85c7e
opacitySun <809340810@qq.com>
2016-07-18 10:55:40 +0800

    更新修改

--- server/models/db.js
+++ server/models/db.js
@@ -2,7 +2,5 @@ var mongodb = require('mongodb');
 var server  = new mongodb.Server('123.57.50.14', 27017, {auto_reconnect:true});
 
 module.exports = {
-	db : function(){
-		return new mongodb.Db('my_blog', server, {safe:true});
-	}
+	return new mongodb.Db('my_blog', server, {safe:true});
 }
