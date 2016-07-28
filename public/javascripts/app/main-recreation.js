f134c18912b99aed3fd6d1ba9786b030d201b5c8
opacitySun <809340810@qq.com>
2016-07-28 12:04:43 +0800

    更新修改

--- public/javascripts/app/main-recreation.js
+++ public/javascripts/app/main-recreation.js
@@ -1,6 +1,40 @@
 define(['require','jquery','fnbase','./controller/c-recreation','./model/m-recreation'],function(require,$,fnbase,controller,model){
 	//获取全部列表
 	model.getAllList(function(res){
-		
+		var html = "";
+    	if(res.result){
+    		$.each(res.result,function(key,obj){
+    			if(obj.type == 0){	//如果是电视剧
+    				html += '<section class="s01">';
+    				html += '<h2>';
+    				html += '<span>'+obj.name+'</span>';
+    				html += '<i>更多></i>';
+    				html += '</h2>';
+    				if(obj.data){
+    					html += '<div class="akey">';
+						html += '<img src="'+obj.data[0].image+'" />';
+						html += '<span>'+obj.data[0].name+'</span>';
+						html += '<em>'+obj.data[0].desc+'</em>';
+						html += '</div>';
+						html += '<ul>';
+	    				$.each(obj.data,function(k,o){
+	    					if(k != 0){
+	    						html += '<li>';
+								html += '<img src="'+o.image+'" />';
+								html += '<span>'+o.name+'</span>';
+								html += '<em>'+o.desc+'</em>';
+								html += '</a>';
+			    				html += '</li>';
+	    					}
+		    			});
+		    			html += '</ul>';
+	    			}
+    				html += '</section>';
+    			}
+    		});
+    	}else{
+    		html = "暂无数据";
+    	}
+    	$("#recreationAll").html(html);
 	});
 });
--- server/controller/recreationController.js
+++ server/controller/recreationController.js
@@ -1,5 +1,6 @@
 var dbHelper = require("../DBHelper/dbHelper");
 var recreationDao = require("../DBSql/recreationDao");
+var recreationTypeDao = require("../DBSql/recreationTypeDao");
 
 module.exports = function(app){
     //获取全部项目列表
@@ -7,16 +8,16 @@ module.exports = function(app){
     	var result = {};
 
         var conditions = {};
-        newsTypeDao.findNewsType(conditions,dbHelper,function(newsTypeResult){  
-            console.log(JSON.stringify(newsTypeResult));
-            result = newsTypeResult;
+        recreationTypeDao.findRecreationType(conditions,dbHelper,function(recreationTypeResult){  
+            console.log(JSON.stringify(recreationTypeResult));
+            result = recreationTypeResult;
             recreationDao.findRecreation(conditions,dbHelper,function(recreationResult){  
             	console.log(JSON.stringify(recreationResult));
             	result.result.forEach(function(obj){
             		var recreationArr = [];
             		recreationResult.result.forEach(function(o){
             			if(obj.type == o.type){
-            				newsArr.push(o);
+            				recreationArr.push(o);
             			}
             		});
             		if(recreationArr.length > 0){
