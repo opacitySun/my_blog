e9a1831f27b470688d7e13adcee07e12dfac1159
opacitySun <809340810@qq.com>
2016-07-28 14:21:27 +0800

    更新修改

--- public/javascripts/app/main-recreation.js
+++ public/javascripts/app/main-recreation.js
@@ -4,7 +4,7 @@ define(['require','jquery','fnbase','./controller/c-recreation','./model/m-recre
 		var html = "";
     	if(res.result){
     		$.each(res.result,function(key,obj){
-    			if(obj.type == 0){	//如果是电视剧
+    			if(obj.type == 0 || obj.type == 1){	//如果是电视剧:0,电影:1
     				html += '<section class="s01">';
     				html += '<h2>';
     				html += '<span>'+obj.name+'</span>';
@@ -12,7 +12,9 @@ define(['require','jquery','fnbase','./controller/c-recreation','./model/m-recre
     				html += '</h2>';
     				if(obj.data){
     					html += '<div class="akey" title="'+obj.data[0].name+'">';
+    					html += '<div class="img">';
 						html += '<img src="'+obj.data[0].image+'" />';
+						html += '</div>';
 						html += '<span>'+obj.data[0].name+'</span>';
 						html += '<em>'+obj.data[0].desc+'</em>';
 						html += '</div>';
@@ -24,12 +26,38 @@ define(['require','jquery','fnbase','./controller/c-recreation','./model/m-recre
 	    						}else{
 	    							html += '<li title="'+o.name+'">';
 	    						}
+	    						html += '<div class="img">';
 								html += '<img src="'+o.image+'" />';
+								html += '</div>';
 								html += '<span>'+o.name+'</span>';
 								html += '<em>'+o.desc+'</em>';
 								html += '</a>';
 			    				html += '</li>';
 	    					}
+		    			});
+		    			html += '</ul>';
+	    			}
+    				html += '</section>';
+    			}else if(obj.type == 2){	//图集
+    				html += '<section class="s02">';
+    				html += '<h2>';
+    				html += '<span>'+obj.name+'</span>';
+    				html += '<i>更多></i>';
+    				html += '</h2>';
+    				if(obj.data){
+						html += '<ul>';
+	    				$.each(obj.data,function(k,o){
+    						if(k == 4){
+    							html += '<li title="'+o.name+'" style="margin-right:0">';
+    						}else{
+    							html += '<li title="'+o.name+'">';
+    						}
+    						html += '<div class="img">';
+							html += '<img src="'+o.image+'" />';
+							html += '</div>';
+							html += '<span>'+o.name+'</span>';
+							html += '</a>';
+		    				html += '</li>';
 		    			});
 		    			html += '</ul>';
 	    			}
