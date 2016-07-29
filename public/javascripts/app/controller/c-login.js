2b4739817266b11b5edd11de9d5dda41a7da872a
opacitySun <809340810@qq.com>
2016-07-29 09:52:35 +0800

    更新修改

--- public/javascripts/app/controller/c-login.js
+++ public/javascripts/app/controller/c-login.js
@@ -3,32 +3,32 @@ define(['./Base'], function (Base) {
     var cLogin = {
         //重置登陆框
         loginReset : function(){
-            $("#login_name").val("");
-            $("#login_pwd").val("");
+            $("#loginName").val("");
+            $("#loginPwd").val("");
         },
         //关闭登陆框
         loginClose : function(){
-            $("#login_dialog").fadeOut();
-            $("#login_box").fadeIn();
+            $("#loginDialog").fadeOut();
+            $("#loginBox").fadeIn();
         },
         //打开登陆框
         loginOpen : function(){
-            $("#login_box").fadeOut();
-            $("#login_dialog").fadeIn();
+            $("#loginBox").fadeOut();
+            $("#loginDialog").fadeIn();
         },
         //底部位置调整
         bottomPosition : function(){
             var screen_h = $(window).height();
             var doc_h = $("body,html").height();
             if(screen_h > doc_h){
-                $(".login_footer").css({
+                $("#loginFooter").css({
                     "float":"none",
                     "position":"fixed",
                     "left":0,
                     "bottom":0
                 });
             }else{
-                $(".login_footer").css({
+                $("#loginFooter").css({
                     "float":"left",
                     "position":"static",
                     "margin-top":"6px"
@@ -43,7 +43,7 @@ define(['./Base'], function (Base) {
         },
         //输入框默认字设置
         defaultText : function(){
-            $("#login_name").focus(function(){
+            $("#loginName").focus(function(){
                 var txt_value = $(this).val();
                 if(txt_value=="Username"){
                     $(this).val("");
@@ -55,7 +55,7 @@ define(['./Base'], function (Base) {
                 }
             });
 
-            $("#login_pwd").focus(function(){
+            $("#loginPwd").focus(function(){
                 var txt_value = $(this).val();
                 if(txt_value=="Password"){
                     $(this).val("");
