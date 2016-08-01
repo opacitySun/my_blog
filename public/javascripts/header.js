f5c136450520ce2023a4656b11a3f330bf8eca05
opacitySun <809340810@qq.com>
2016-08-01 16:00:14 +0800

    更新修改

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,28 +1,5 @@
 
-define(['require','jquery','fnbase'],function(require,$,fnbase){
-	//创建头部
-	var html = "";
-	html += '<div class="header_top">';
-	html += '	<div class="top_center">';
-	html += '		<div class="logo">SUN</div>';
-	html += '		<ul>';
-	html += '		<li id="setHome"><a href="javascript:void(0)" title="设为首页">设为首页</a></li>';
-	html += '		<span>|</span>';
-	html += '		<li id="addFavorite"><a href="javascript:void(0)" title="收藏本站">收藏本站</a></li>';
-	html += '		</ul>';
-	html += '	</div>';
-	html += '</div>';
-	html += '<nav>';
-	html += '	<ul>';
-	html += '		<li><a href="/" title="网站首页">网站首页</a></li>';
-	html += '		<li><a href="/works-list" title="我的作品">我的作品</a></li>';
-	html += '		<li><a href="/study-list" title="学习分享">学习分享</a></li>';
-	html += '		<li><a href="/recreation" title="我的娱乐">我的娱乐</a></li>';
-	html += '		<li><a href="/news-list" title="消息新闻">消息新闻</a></li>';
-	html += '	</ul>';
-	html += '</nav>';
-	$("#header").html(html);
-
+require(['require','jquery','fnbase'],function(require,$,fnbase){
 	//设置菜单选中状态
 	var url = window.location.href;
 	var navLiNo = 0;
