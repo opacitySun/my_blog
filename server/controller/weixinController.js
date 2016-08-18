0edcf8c84ac30496c300f85acea32f2794e0d974
opacitySun <809340810@qq.com>
2016-08-18 11:59:00 +0800

    更新修改

--- server/controller/weixinController.js
+++ server/controller/weixinController.js
@@ -1,4 +1,6 @@
-var wechat = require('wechat');
+var http = require("http");
+var url = require("url");
+var crypto = require("crypto");
 
 /**  
  * 提供操作表的公共路由，以供ajax访问  
@@ -15,19 +17,45 @@ exports.outerConnectAction = function(app){
 }
 
 /**  
- * add user  
- * @returns {Function}  
- */  
-exports.userAddAction = function() {  
-    return function(req, res) {  
-        res.json(result);  
-    }  
-}  
-
-/**  
  * get User  
  * @returns {Function}  
  */  
-exports.userFindAction = function(req, res) {   
-    //res.json(result);  
+exports.validateToken = function(req, res, next) {   
+    function sha1(str){
+      var md5sum = crypto.createHash("sha1");
+      md5sum.update(str);
+      str = md5sum.digest("hex");
+      return str;
+    }
+
+    function validateToken(req,res){
+      var query = url.parse(req.url,true).query;
+      //console.log("*** URL:" + req.url);
+      //console.log(query);
+      var signature = query.signature;
+      var echostr = query.echostr;
+      var timestamp = query['timestamp'];
+      var nonce = query.nonce;
+      var oriArray = new Array();
+      oriArray[0] = nonce;
+      oriArray[1] = timestamp;
+      oriArray[2] = "sunxxjjs8ceow90xc92";//这里是你在微信开发者中心页面里填的token
+      oriArray.sort();
+      var original = oriArray.join('');
+      console.log("Original str : " + original);
+      console.log("Signature : " + signature );
+      var scyptoString = sha1(original);
+      if(signature == scyptoString){
+        res.end(echostr);
+        console.log("Confirm and send echo back");
+      }else {
+        res.end("false");
+        console.log("Failed!");
+      }
+    }
+
+    var webSvr = http.createServer(validateToken);
+    webSvr.listen(80,function(){
+      console.log("Start validate");
+    });
 }  
