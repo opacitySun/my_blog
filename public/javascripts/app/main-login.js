4e08b2bfca3cc7b515d496beec6cab3c81c48f20
opacitySun <809340810@qq.com>
2016-08-22 16:32:04 +0800

    更新修改

--- public/javascripts/app/main-login.js
+++ public/javascripts/app/main-login.js
@@ -1,5 +1,10 @@
 
 define(['require','jquery','./controller/c-login','./model/m-login'],function(require,$,controller,model){
+    $("#loginDialog").on("keydown",function(event){
+        if(event.keyCode == 13){
+            controller.loginSubmit();
+        }
+    });
     $("#loginSubmit").on("click",function(){
         controller.loginSubmit();
     });
