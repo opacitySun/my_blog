c55082c2a250038d3baec004de2b2c8631395290
opacitySun <809340810@qq.com>
2016-08-22 12:15:49 +0800

    更新修改

--- public/javascripts/lib/easing.js
+++ public/javascripts/lib/easing.js
@@ -11,6 +11,7 @@
 
 // t: current time, b: begInnIng value, c: change In value, d: duration
 
+define(['jquery'],function(jQuery){
 jQuery.extend( jQuery.easing,
 {
 	easeInQuad: function (x, t, b, c, d) {
@@ -137,4 +138,5 @@ jQuery.extend( jQuery.easing,
 		if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
 		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
 	}
+});
 });
