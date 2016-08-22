8b9b716c6bcd9a7f6d4bcd06006dd27c20177366
opacitySun <809340810@qq.com>
2016-08-22 12:22:46 +0800

    更新修改

--- public/javascripts/app/controller/c-work.js
+++ public/javascripts/app/controller/c-work.js
@@ -2,6 +2,13 @@ define(['./Base','hoverdir'], function (Base,hoverdir) {
 	var cWork = {
 		jqHoverdir : function(){
 			$('#worksList > li').each( function() { $(this).hoverdir(); } );
+		},
+		//菜单按钮
+		menuButton : function(){
+			$("#menuButton a").on("click",function(){
+				$(this).parent().find("a").removeClass("current");
+				$(this).addClass("current");
+			});
 		}
 	};
 
--- public/javascripts/app/main-work.js
+++ public/javascripts/app/main-work.js
@@ -1,6 +1,9 @@
 
 define(['require','jquery','./controller/c-work','./model/m-work'],function(require,$,controller,model){
 	var staticPath = $("#staticPath").val();
+
+	//菜单按钮
+	controller.menuButton();
 	
     //获取全部项目列表
     model.getAllList(function(res){
