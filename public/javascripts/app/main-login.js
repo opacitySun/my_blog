05f6d682c3444ee7a7fd6d1c32d5cd4eaa002f8b
opacitySun <809340810@qq.com>
2016-08-22 15:58:16 +0800

    更新修改

--- public/javascripts/app/main-login.js
+++ public/javascripts/app/main-login.js
@@ -1,34 +1,6 @@
 
 define(['require','jquery','./controller/c-login','./model/m-login'],function(require,$,controller,model){
-    /*
-    var controller = require('./controller/c-login'),
-        model = require('./model/m-login');
-        */
-
-    controller.bottomPosition();
-    controller.contentHeight();
-    //controller.defaultText();
-
-    $("#loginReset").click(function(){
-        controller.loginReset();
-    });
-    $("#loginClose").click(function(){
-        controller.loginClose();
-    });
-    $("#loginOpen").click(function(){
-        controller.loginOpen();
-    });
-    $("#loginDialog").on("keydown",function(event){
-        if(event.keyCode == 13){
-            controller.loginSubmit();
-        }
-    });
     $("#loginSubmit").on("click",function(){
         controller.loginSubmit();
     });
-
-    window.onresize = function(){
-        controller.bottomPosition();
-        controller.contentHeight();
-    }
 });
