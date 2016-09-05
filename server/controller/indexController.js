130753a32e1bc6cffca22099621ef818acb569f8
opacitySun <809340810@qq.com>
2016-09-05 15:37:40 +0800

    更新修改

--- server/controller/indexController.js
+++ server/controller/indexController.js
@@ -1,15 +1,23 @@
 var dbHelper = require("../DBHelper/dbHelper");
+var userDao = require("../DBSql/userDao");
 var userWorksDao = require("../DBSql/userWorksDao");
 var newsDao = require("../DBSql/newsDao");
 
 module.exports = function(app){
     //获取作品列表
     app.all("/userWorksFindAction",function(req,res){
-        var conditions = {};
-        userWorksDao.findUserWorks(conditions,dbHelper,function(result){  
-            console.log(JSON.stringify(result));
-            res.json(result);
-        });    
+        var conditions0 = {
+            "name":req.session.username,
+            "password":req.session.password
+        };
+        userDao.findOneUser(conditions0,dbHelper,function(result0){
+            var conditions1 = {
+                "userId":result0.result._id.toString()
+            };
+            userWorksDao.findUserWorks(conditions1,dbHelper,function(result1){  
+                res.json(result1);
+            });    
+        });   
     });
 
     //获取新闻列表
