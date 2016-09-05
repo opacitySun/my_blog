eda8c466ed5d1bb54d01ef7a5882edba76e686cc
opacitySun <809340810@qq.com>
2016-09-05 15:22:51 +0800

    更新修改

--- server/controller/worksController.js
+++ server/controller/worksController.js
@@ -1,23 +1,41 @@
 var dbHelper = require("../DBHelper/dbHelper");
 var userWorksDao = require("../DBSql/userWorksDao");
+var userDao = require("../DBSql/userDao");
 
 module.exports = function(app){
     //获取项目列表
     app.all("/worksListFindAction",function(req,res){
-    	//type:0是PC,1是手机;status:0是在线,1是静态
-        var conditions = {"type":Number(req.body.type),"status":Number(req.body.status)};
-        userWorksDao.findUserWorks(conditions,dbHelper,function(result){  
-            console.log(JSON.stringify(result));
-            res.json(result);
-        });    
+        var conditions0 = {
+            "name":req.session.username,
+            "password":req.session.password
+        };
+        userDao.findOneUser(conditions0,dbHelper,function(result0){
+            //type:0是PC,1是手机;status:0是在线,1是静态
+            var conditions1 = {
+                "userId":result0.result._id.toString(),
+                "type":Number(req.body.type),
+                "status":Number(req.body.status)
+            };
+            userWorksDao.findUserWorks(conditions1,dbHelper,function(result1){  
+                res.json(result1);
+            });    
+        });
     });
 
     //获取全部项目列表
     app.all("/worksAllListFindAction",function(req,res){
-        var conditions = {};
-        userWorksDao.findUserWorks(conditions,dbHelper,function(result){  
-            console.log(JSON.stringify(result));
-            res.json(result);
-        });    
+        var conditions0 = {
+            "name":req.session.username,
+            "password":req.session.password
+        };
+        userDao.findOneUser(conditions0,dbHelper,function(result0){
+            //type:0是PC,1是手机;status:0是在线,1是静态
+            var conditions1 = {
+                "userId":result0.result._id.toString()
+            };
+            userWorksDao.findUserWorks(conditions1,dbHelper,function(result1){  
+                res.json(result1);
+            });    
+        });   
     });
 }
