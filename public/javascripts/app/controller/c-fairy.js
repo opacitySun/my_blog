46a6fd9b7e8ca9eeebc9c841f27e54d1f518cfc5
opacitySun <809340810@qq.com>
2016-09-06 17:35:16 +0800

    更新修改

--- public/javascripts/app/controller/c-fairy.js
+++ public/javascripts/app/controller/c-fairy.js
@@ -3,25 +3,6 @@ define(["jquery","bootstrap","../model/m-fairy"],function($,bootstrap,model){
     var staticPath = $("#staticPath").val();
 
 	var cFairy = {
-        //获取作品列表
-        getWorksList : function(){
-            model.getUserWorks(function(res){
-                var html = "";
-                if(res.result){
-                    $.each(res.result,function(key,obj){
-                        html += '<li>';
-                        html += '<div class="biseller-column">';
-                        html += '<a class="lightbox" target="_blank" href="'+obj.workUrl+'" title="'+obj.workName+'">';
-                        html += '<img src="'+staticPath+obj.workImg+'" alt="'+obj.workName+'" />';
-                        html += '</a>';
-                        html += '</div>';
-                        html += '</li>';
-                    });
-                }
-                $("#userWorks").html(html);
-                cIndex.flexisel();
-            });
-        },
         //判断是否显示精灵
         fairyShow : function(){
             model.hasFairy(function(resHas){
@@ -29,7 +10,7 @@ define(["jquery","bootstrap","../model/m-fairy"],function($,bootstrap,model){
                     var id = resHas.result._id.toString();
                     model.getFairyAllInfoById(id,function(res){
                         if(res.success == 1){
-                            var typeImage = '<img src="'+res.result.image+'" />';
+                            var typeImage = '<img src="'+staticPath+res.result.image+'" />';
                             $("#myfairy .bottom").html(typeImage);
                             $("#myfairy").show();
                         }else{
