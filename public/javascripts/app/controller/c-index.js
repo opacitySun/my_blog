c7da07e782854d174a55aadf2a5c533b5285634b
opacitySun <809340810@qq.com>
2016-09-06 11:22:59 +0800

    更新修改

--- public/javascripts/app/controller/c-index.js
+++ public/javascripts/app/controller/c-index.js
@@ -1,5 +1,5 @@
 
-define(["jquery","flexisel","../model/m-index"],function($,flexisel,model){
+define(["jquery","bootstrap","flexisel","../model/m-index"],function($,bootstrap,flexisel,model){
     var staticPath = $("#staticPath").val();
 
 	var cIndex = {
@@ -44,37 +44,6 @@ define(["jquery","flexisel","../model/m-index"],function($,flexisel,model){
                 }
                 $("#userWorks").html(html);
                 cIndex.flexisel();
-            });
-        },
-        //判断是否显示选择精灵弹出窗
-        fairyModal : function(){
-            model.hasFairy(function(resHas){
-                if(resHas.success != 1){
-                    model.getFairyTypeList(function(res){
-                        if(res.success == 1){
-                            var html = "";
-                            $.each(res.result,function(key,obj){
-                                html += '<li>';
-                                html += '<input type="checkbox" name="type" value="'+obj.type+'" />';
-                                html += '<img src="'+staticPath+obj.image+'" />';
-                                html += '</li>';
-                            });
-                            $("#fairyListByModal").html(html);
-                            cIndex.selFairy();
-                            $("#selFairyModal").modal("show");
-                        }else{
-                            console.log(res);
-                        }   
-                    });
-                }
-            });
-        },
-        //精灵选中效果
-        selFairy : function(){
-            $("#fairyListByModal li").on("click",fucntion(){
-                $(this).find("input[type='checkbox']").attr("checked",true);
-                $(this).siblings().find("input[type='checkbox']").attr("checked",false);
-                $(this).addClass("current").siblings().removeClass("current");
             });
         }
 	};
--- public/javascripts/app/main-index.js
+++ public/javascripts/app/main-index.js
@@ -1,4 +1,4 @@
-define(['jquery','fnbase','bootstrap','./controller/c-index','./model/m-index'],function($,fnbase,bootstrap,controller,model){
+define(['jquery','fnbase','./controller/c-index','./model/m-index'],function($,fnbase,controller,model){
     //获取作品列表
     controller.getWorksList();
     //判断是否显示选择精灵弹出窗
