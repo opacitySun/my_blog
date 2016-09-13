8f843c2558af6c84fec3b3abfca17504480e326e
opacitySun <809340810@qq.com>
2016-09-13 11:49:16 +0800

    更新修改

--- public/javascripts/app/controller/c-fairy.js
+++ public/javascripts/app/controller/c-fairy.js
@@ -13,7 +13,7 @@ define(["jquery","bootstrap","../model/m-fairy"],function($,bootstrap,model){
                             var fairyBottom = '';
                             fairyBottom += '<div class="level">';
                             fairyBottom += '<p>LV.'+res.result.level+'</p>';
-                            fairyBottom += '<div class="exp">';
+                            fairyBottom += '<div class="exp" id="exp">';
                             fairyBottom += '<b></b>';
                             fairyBottom += '<em>'+res.result.exp+'/'+res.result.nextExp+'</em>';
                             fairyBottom += '</div>';
@@ -23,9 +23,9 @@ define(["jquery","bootstrap","../model/m-fairy"],function($,bootstrap,model){
 
                             //设置经验值
                             var expPercent = res.result.exp/res.result.nextExp;
-                            var expWidth = $(".level .exp").width();
+                            var expWidth = $("#exp").width();
                             var bgWidth = expPercent * expWidth;
-                            $(".level .exp b").animate({"width":bgWidth+"px"});
+                            $("#exp b").animate({"width":bgWidth+"px"});
 
                             $("#myfairy .txtBox p").html(res.result.desc);
                             $("#myfairy").show();
